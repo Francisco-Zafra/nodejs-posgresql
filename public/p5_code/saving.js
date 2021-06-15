6f751ad6862fd126bb11ff0b54e26a97f99d6beb
Francisco-Zafra <franciscoz1999@gmail.com>
2021-06-15 12:07:06 +0200

    builds

--- public/p5_code/saving.js
+++ public/p5_code/saving.js
@@ -4,7 +4,17 @@ class Save{
     }
 
     guardar(){
-               
+      var fs = require('fs');
+      // convert JSON object to string
+      var data = JSON.stringify("mapa");
+      console.log(data);
+      fs.writeFile('save.json', data, err => {
+          if (err) {
+          console.error(err);
+          return;
+          }
+          console.log("Json guardado");
+      })           
     }
 
     cargar(){
