452e4be299a7873e013629b83a4f2a98fe57c9b4
Francisco-Zafra <franciscoz1999@gmail.com>
2021-06-30 16:57:13 +0200

    ahora?

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const http = require('http');
-var fs = require('fs');
 var express = require('express');
-var bodyParser = require('body-parser');
-const { nextTick } = require('process');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var quotesRouter = require('./routes/quotes');
 
 var app = express();
 
-app.use(express.static('p5_code'));
-app.use(express.static('public'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/quotes', quotesRouter);
+
 app.use(bodyParser.json({limit: '200mb'}));
 app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
 
@@ -22,50 +31,4 @@ app.get('/', function(req, res) {
   return
 });
 
-app.post('/guardar', function(req, res) {
-  // var mapa = JSON.parse(req.body.value);
-  var mapa = req.body
-  fs.writeFile("mapa.json", JSON.stringify(mapa), function (err) {
-    if (err) return console.log(err);
-  });
-  res.send("OK");
-});
-
-app.post('/guardar2', function(req, res) {
-  // var mapa = JSON.parse(req.body.value);
-  var path = req.body
-  fs.writeFile("path.json", JSON.stringify(path), function (err) {
-    if (err) return console.log(err);
-  });
-  res.send("OK");
-});
-
-app.get('/cargar', function(req, res) {
-  // var mapa = JSON.parse(req.body.value);
-  fs.readFile('mapa.json', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    res.send(data);
-  })
-  //res.end();
-  return
-});
-
-app.get('/cargar2', function(req, res) {
-  // var mapa = JSON.parse(req.body.value);
-  fs.readFile('path.json', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    res.send(data);
-  })
-  //res.end();
-  return
-});
-
-app.listen(3000, function() {
-  console.log('Self Avoiding Walk, escuchando el puerto 3000!');
-});
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,48 @@ router.get('/', function(req, res, next) {
   res.json({message: 'alive'});
 });
 
+app.post('/guardar', function(req, res) {
+  // var mapa = JSON.parse(req.body.value);
+  var mapa = req.body
+  fs.writeFile("mapa.json", JSON.stringify(mapa), function (err) {
+    if (err) return console.log(err);
+  });
+  res.send("OK");
+});
+
+app.post('/guardar2', function(req, res) {
+  // var mapa = JSON.parse(req.body.value);
+  var path = req.body
+  fs.writeFile("path.json", JSON.stringify(path), function (err) {
+    if (err) return console.log(err);
+  });
+  res.send("OK");
+});
+
+app.get('/cargar', function(req, res) {
+  // var mapa = JSON.parse(req.body.value);
+  fs.readFile('mapa.json', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.send(data);
+  })
+  //res.end();
+  return
+});
+
+app.get('/cargar2', function(req, res) {
+  // var mapa = JSON.parse(req.body.value);
+  fs.readFile('path.json', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.send(data);
+  })
+  //res.end();
+  return
+});
+
 module.exports = router;
